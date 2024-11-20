8a06a9fc917acbf13a384a3c72e3787eef454794
KalleStephensen <kallestephensen@gmail.com>
2024-11-20 15:09:35 +0100

    Add files via upload

--- Pizzaspil/script.js
+++ Pizzaspil/script.js
@@ -20,7 +20,7 @@ const startScenario = {
     option4:"",
 }
 const scenario1 = {
-    scenarioImage:'images/s1.jpg',
+    scenarioImage:'images/s1.png',
     text:'The night is dark and the streets of Pizzonia are almost empty. The bussteling city had fallen into it’s usual slumber. As you walk through the quiet streets you notice a dimly lit alleyway, with a peculiar looking creature, standing at the end of it... It was a rat holding a piece of pizza.',
     option1:"> Approach the rat",
     option2:"",
@@ -36,7 +36,7 @@ const scenario2 = {
     option4:"",
 }
 const scenario3 = {
-    scenarioImage:'images/s2.png',
+    scenarioImage:'images/s3.png',
     text:'“Great choice! Now let’s see if you’ve got good pizza taste or not… Which type of crust would you like?”\n\nYou’re presented with the following options:',
     option1:"> Classic",
     option2:"> Glutenfree",
@@ -44,7 +44,7 @@ const scenario3 = {
     option4:"",
 }
 const scenario4 = {
-    scenarioImage:'images/s2.png',
+    scenarioImage:'images/s4.png',
     text:'“Splendid choice! Now we arrive at the juiciest part, or should I say the sauciest one?”',
     option1:"> Tomato Sauce",
     option2:"> Garlic with a splash of oil",
@@ -52,31 +52,31 @@ const scenario4 = {
     option4:"",
 }
 const scenario5 = {
-    scenarioImage:'images/s2.png',
+    scenarioImage:'images/s5.png',
     text:'“Well well well... Now I need to know if you like meat or not. I do want to make the perfect pizza for you after all.”',
     option1:"> Vegetarian",
-    option2:"> Is that even a question!!!",
+    option2:"> Is that even a question!? Meat all the way!",
     option3:"",
     option4:"",
 }
 const scenario61 = {
-    scenarioImage:'images/s2.png',
-    text:'Jeg har også hørt rygter om at grøntsager skulle være sunde. Jeg tror personligt ikke selv på det, men det må du selv om.',
-    option1:"> Veggie Super (Ost, Champignon, Peberfrugt, Løg, Artiskokker, Oliven)",
-    option2:"> Spinat (Ost, Spinat, Gorgonzola, Hvidløg)",
+    scenarioImage:'images/s61.png',
+    text:'“I’ve also heard rumors that vegetables are supposed to be healthy. Personally, I don’t buy it, but hey, that’s your choice.“',
+    option1:"> Veggie Super (Cheese, Mushrooms, Bell Peppers, Onions, Artichokes, Olives)",
+    option2:"> Spinach (Cheese, Spinach, Gorgonzola, Garlic)",
     option3:"",
     option4:"",
 }
 const scenario62 = {
-    scenarioImage:'images/s2.png',
-    text:'Godt valg! Det sunde valg! Hvilken type kød-pizza vil du have?',
+    scenarioImage:'images/s62.png',
+    text:'“Ah, great choice! The healthy choice, eh? But c’mon, let’s not kid ourselves—you came here for the meat, didn’t you? I got the good stuff, don’t worry! Let’s load you up proper!“',
     option1:"> BBQ KYLLING PIZZA (Ost, BBQ kylling, Ananas, Champignon, Karry)",
     option2:"> KØDBOMBEN (Bacon, Kylling, Kebab, Peperoni, Skinke)",
     option3:"> SANTA MARIA (Ost, Oksekød, Skinke, Bacon, Pølser)",
     option4:"> MARINARA (Ost, Tun, Rejer, Muslinger, Oliven)",
 }
 const scenario7 = {
-    scenarioImage:'images/s2.png',
+    scenarioImage:'images/s7.png',
     text:'',
     option1:"> Continue",
     option2:"",
@@ -84,31 +84,31 @@ const scenario7 = {
     option4:"",
 }
 const scenario8 = {
-    scenarioImage:'images/s2.png',
-    text:'Ratfael spørger om betaling.',
-    option1:"> Betal Ratfael",
-    option2:"> Du får ikke en rød reje!!!",
+    scenarioImage:'images/s8.png',
+    text:'“Now hand over the money pipsqeak!”',
+    option1:"> Pay Ratfael",
+    option2:"> Your are not getting a dime from me, you sleezy rat!",
     option3:"",
     option4:"",
 }
 const scenario91 = {
-    scenarioImage:'images/s2.png',
-    text:'En fornøjelse at gøre fortening med dig. Pizzaen er klar om 10 minutter.',
-    option1:"> ",
+    scenarioImage:'images/s9.png',
+    text:'“A pleasure doing business with you. Your pizza will be ready in 10 minutes.”',
+    option1:"> Wait for the pizza",
     option2:"",
     option3:"",
     option4:"",
 }
 const scenario92 = {
-    scenarioImage:'images/s2.png',
-    text:'Fuck dig du får ingen pizza af mig!!!',
-    option1:"> Flygt fra Pizzonia",
+    scenarioImage:'images/s92.png',
+    text:'“Trying to be cool now ey? You are gonna regret that!” Ratfaels eyes roll into the back of his head and his mouth began to widen. Something started to move underneath his skin. The creature trapped inside this coffin of flesh was trying to get out. It slowly ripped his body apart from the inside. Where Ratfael once stood, there was now a beast with tentacles for eyes and a mouth full of eyeballs all starring directly at me.',
+    option1:"> Escape from Pizzonia",
     option2:"",
     option3:"",
     option4:"",
 }
 const endScenario1 = {
-    scenarioImage:'images/s2.png',
+    scenarioImage:'images/ends1.png',
     text:'',
     option1:"",
     option2:"",
@@ -116,7 +116,7 @@ const endScenario1 = {
     option4:"",
 }
 const endScenario2 = {
-    scenarioImage:'images/placeholder.jpg',
+    scenarioImage:'images/ends2.png',
     text:'',
     option1:"",
     option2:"",
@@ -304,5 +304,5 @@ function updateCurrentScenario() {
 }
 
 function scenario7Text() {
-    scenario7.text = 'Det lyder fandme som en lækker pizza! En ' + pizzabund + ' pizzabund med ' + sauce + '. Godt valg med en ' + toppingType + ' pizza. Det lyder satme også frækt med ' + topping + '.'
+    scenario7.text = '“That sounds like one hell of a delicious pizza! A ' + pizzabund + ' base with ' + sauce + '. Great choice going for a ' + toppingType + ' pizza. And that ' + topping + ' — now that sounds downright naughty!”'
 }
